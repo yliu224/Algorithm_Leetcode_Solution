033161c23ba1803d2776abb24adcd4aeda3bdcf7
dean <yliu224@dons.usfca.edu>
2017-01-18 22:54:55 -0800

    leetcod61

--- src/FIGHTING/Main.java
+++ src/FIGHTING/Main.java
@@ -1,12 +1,13 @@
 package FIGHTING;
 
 import FIGHTING.Leetcode.String.leetcode3;
+import FIGHTING.Leetcode.leetcode61;
 
 public class Main {
 
     public static void main(String[] args) {
-        leetcode3 s=new leetcode3();
-        s.lengthOfLongestSubstring("abcdcb");
+        leetcode61 l61=new leetcode61();
+        l61.rotateRight(BuildHelper.CreateListNode("[1,2,3,4,5]"),3);
     }
 
 }
